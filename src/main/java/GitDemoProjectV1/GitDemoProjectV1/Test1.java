64cfb00e799949a8642d11a1dba81c28b891a4e4
91827 <91827@Abhishek>
2022-02-25 19:31:19 +0530

    First commit

---
+++ src/main/java/GitDemoProjectV1/GitDemoProjectV1/Test1.java
@@ -0,0 +1,13 @@
+package GitDemoProjectV1.GitDemoProjectV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
